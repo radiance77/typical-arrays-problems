a16b4748fca013e4bd58bd6b574191f827746dfd
angelina <floresangela.0707@gmail.com>
2021-08-08 21:07:32 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ exports.min = function min (array) {
     if(array === undefined || array.length === 0){
         return 0;
     }
-
     let minElem = Math.min(...array);
     return minElem;
 }
@@ -19,5 +18,5 @@ exports.avg = function avg (array) {
     if(array === undefined || array.length === 0){
         return 0;
     }
-    return (array.reduce((a,b) => a+b, 0)) / array.length;
+    return (array.reduce((a,b) => a+b)) / array.length;
 }
