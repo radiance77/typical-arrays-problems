0814a0fac1351213609f9d728718a064894603a7
angelina <floresangela.0707@gmail.com>
2021-08-08 21:03:37 +0600

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+
+    let minElem = Math.min(...array);
+    return minElem;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+    let maxElem = Math.max(...array);
+    return maxElem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    }
+    return (array.reduce((a,b) => a+b, 0)) / array.length;
 }
